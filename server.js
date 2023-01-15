250579d0de5d7625c099ba23aa6f0d4825afe4d2
Emiliya Aghayeva <ag.emiliya@gmail.com>
2023-01-14 22:48:10 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: _Emiliya Aghayeva_____________________ Student ID: _148398217_____________ Date: ____14/01/2023____________
+*
+*  Online (Cyclic) URL: ___https://faithful-pantsuit-ox.cyclic.app____________________________________________________
+*
+********************************************************************************/ 
+
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
